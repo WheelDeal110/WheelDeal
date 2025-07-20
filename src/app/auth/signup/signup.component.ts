cd5ac34019f5153e6c7ffbe2a6fac1cda9348460
Sami907 <ssam70207@gmail.com>
2025-07-21 01:03:04 +0500

    use sign up api

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { catchError, finalize, of } from 'rxjs';
+import { AuthService } from 'src/app/appServices/auth.service';
+import { AlertComponent } from 'src/app/shared/alert/alert.component';
 
 @Component({
   selector: 'app-signup',
@@ -9,11 +12,14 @@ import { Router } from '@angular/router';
 })
 export class SignupComponent {
    signupForm!: FormGroup;
+  @ViewChild(AlertComponent) alertRef!: AlertComponent;
+  isLoading:boolean = false;
 
-  constructor(private fb: FormBuilder,private router:Router) {}
+  constructor(private fb: FormBuilder,private router:Router,private auth:AuthService) {}
 
   ngOnInit() {
     this.signupForm = this.fb.group({
+      fullname:['',[Validators.required]],
       phone: ['', [Validators.required, Validators.pattern(/^\+?\d{10,15}$/)]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', Validators.required],
@@ -27,13 +33,36 @@ export class SignupComponent {
     return pass === confirmPass ? null : { passwordMismatch: true };
   }
 
-  onSubmit() {
-    this.router.navigate(['controlPanel']);
+  async onSubmit() {
     if (this.signupForm.invalid) {
       this.signupForm.markAllAsTouched();
       return;
     }
-    console.log('Signup Data:', this.signupForm.value);
-    // Your signup logic here
-  }
+
+    this.isLoading = true;
+
+    const payload = {
+        name:this.signupForm.get('fullname')?.value,
+        email:this.signupForm.get('email')?.value,
+        phone:this.signupForm.get('phone')?.value,
+        passwordHash:this.signupForm.get('password')?.value
+    }
+
+      this.auth.createAccount(payload).pipe(
+        finalize(() => this.isLoading = false),
+        catchError((error) => {
+          const errorMsg = error?.error?.errorMessages || 'Failed to signup.';
+          this.alertRef.showAlert('error', errorMsg);
+          return of(null);
+        })
+      ).subscribe((res: any) => {
+        if (res?.isSuccess) {
+          this.alertRef.showAlert('success', res?.messages);
+          this.router.navigate(['/controlPanel']);
+        }
+        else{
+          this.alertRef.showAlert('error', res?.errorMessages);
+        }
+      });
+    }
 }
