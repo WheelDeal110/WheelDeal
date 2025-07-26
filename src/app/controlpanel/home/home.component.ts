5d4a16c99dd39255e359364234323d99f6f4d379
Sami907 <ssam70207@gmail.com>
2025-07-27 04:37:01 +0500

    implement logout functionality

--- src/app/controlpanel/home/home.component.ts
+++ src/app/controlpanel/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/appServices/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-showAccounts = false;
+  showAccounts = false;
 
+  constructor(private auth:AuthService) {
+    
+  }
+  
   toggleAccounts() {
     this.showAccounts = !this.showAccounts;
+  }
+
+  logout()
+  {
+    this.auth.logoutCurrentSession();
   }
 }
