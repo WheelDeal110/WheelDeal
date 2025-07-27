2866a0f62dcc416b571f536acbf4e4175477cfec
Sami907 <ssam70207@gmail.com>
2025-07-27 05:22:31 +0500

    implement set and get userid method

--- src/app/appServices/auth.service.ts
+++ src/app/appServices/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { BehaviorSubject, Observable } from "rxjs";
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,15 +11,17 @@ export class AuthService {
   private tokenKey = 'UCQerJKKljTUXg7YAwjYDJArhKRp36FCY2qQ1OQj9m';
   private currentUserSubject = new BehaviorSubject<string | null>(this.getToken());
   private apiUrl = 'https://wheeldeal-001-site1.qtempurl.com/api/Home/';
+  private userIdKey = 'userId';
 
   constructor(private router: Router, private http: HttpClient) {
     
   }
 
   logoutCurrentSession(): void {
+    localStorage.clear();
     localStorage.removeItem(this.tokenKey);
     this.currentUserSubject.next(null);
-    this.router.navigate(['/auth/login']);
+    this.router.navigate(['/']);
   }
 
   get currentToken$(): Observable<string | null> {
@@ -51,6 +54,14 @@ export class AuthService {
   isAuthenticated(): boolean {
     const token = this.getToken();
     return !!token && !this.isTokenExpired();
+  }
+
+  setUserId(id: string) {
+    localStorage.setItem(this.userIdKey, id);
+  }
+
+  getUserId(): string | null {
+    return localStorage.getItem(this.userIdKey);
   }
 
   createAccount(paramsObject: any): Observable<any> {
